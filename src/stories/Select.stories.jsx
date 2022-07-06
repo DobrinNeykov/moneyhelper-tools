fd0f8a01e6175e1575c97ac96a845de812cc5b13
Andrew Vos <andrew.vos@gmail.com>
2022-07-06 15:11:57 +0100

    Move all package stuff to src/lib

--- src/stories/Select.stories.jsx
+++ src/stories/Select.stories.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Select } from "./Select";
+import { Select } from "../lib/Select";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
