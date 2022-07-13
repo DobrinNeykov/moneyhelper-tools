e0aec39e0bb8a487c55cd27b483fe0ffae2ab4a4
Andrew Vos <andrew.vos@gmail.com>
2022-07-13 11:37:57 +0100

    Add a custom colour

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: {
+          700: "#d00000",
+        },
+      },
+    },
   },
   plugins: [],
   corePlugins: {
