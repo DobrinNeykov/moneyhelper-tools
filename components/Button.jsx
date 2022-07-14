da3a6cf7c73794f03f4244996c11e17fa57ba682
Andrew Vos <andrew.vos@gmail.com>
2022-07-14 15:47:37 +0100

    Switch button to tailwind

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,36 +1,82 @@
 import React from "react";
 import PropTypes from "prop-types";
-import styles from "./Button.module.css";
+import classNames from "classnames";
 
 /**
  * Primary UI component for user interaction
  */
 export const Button = ({ primary, disabled, label, autoFocus, ...props }) => {
-  if (primary) {
-    return (
-      <button
-        disabled={disabled}
-        className={styles.primary}
-        autoFocus={autoFocus}
-        type="submit"
-        {...props}
-      >
-        <div className={styles.content}>{label}</div>
-      </button>
-    );
-  } else {
-    return (
-      <button
-        disabled={disabled}
-        className={styles.secondary}
-        autoFocus={autoFocus}
-        type="submit"
-        {...props}
-      >
-        <div className={styles.content}>{label}</div>
-      </button>
-    );
-  }
+  const commonClasses = [
+    "t2",
+    "cursor-pointer",
+    "p-3",
+    "font-bold",
+    "rounded-md",
+    "outline-none",
+    "shadow-bottom-gray",
+
+    "disabled:cursor-not-allowed",
+    "disabled:border-slate-400",
+    "disabled:border",
+    "disabled:bg-transparent",
+    "disabled:text-gray-500",
+    "disabled:shadow-none",
+  ];
+
+  const primaryClasses = commonClasses.concat([
+    "border-0",
+    "text-white",
+    "bg-pink-600",
+
+    "hover:bg-pink-800",
+
+    "active:bg-pink-900",
+    "active:border-yellow-200",
+    "active:border-2",
+    "active:shadow-none",
+
+    "focus:bg-yellow-200",
+    "focus:border-purple-700",
+    "focus:border-2",
+    "focus:text-gray-800",
+    "focus:shadow-none",
+  ]);
+
+  const secondaryClasses = commonClasses.concat([
+    "border",
+    "border-pink-600",
+    "text-pink-600",
+    "bg-white",
+    "shadow-bottom-gray",
+
+    "hover:border",
+    "hover:border-pink-800",
+    "hover:text-pink-800",
+    "hover:shadow-none",
+
+    "active:text-gray-800",
+    "active:border-purple-700",
+    "active:border-2",
+    "active:shadow-none",
+
+    "focus:bg-yellow-200",
+    "focus:border-purple-700",
+    "focus:border-2",
+    "focus:text-gray-800",
+    "focus:shadow-none",
+  ]);
+
+  return (
+    <button
+      disabled={disabled}
+      className={classNames(primary ? primaryClasses : secondaryClasses)}
+      autoFocus={autoFocus}
+      type="submit"
+      {...props}
+    >
+      {label}
+    </button>
+  );
 };
 
 Button.propTypes = {
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ module.exports = {
         red: {
           700: "#d00000",
         },
+        yellow: {
+          200: "#F0F05A",
+        },
         gray: {
           100: "#f3f1f3",
           800: "#000b3b",
@@ -17,6 +20,14 @@ module.exports = {
         purple: {
           700: "#8200D1",
         },
+        pink: {
+          600: "#c82a87",
+          800: "#ae0060",
+          900: "#9c0052",
+        },
+      },
+      boxShadow: {
+        "bottom-gray": "0px 3px 0px rgba(0, 11, 59, 0.25)",
       },
     },
   },
