3cfb7a828e3958eb48cd5b0e185784d63bb96db6
Andrew Vos <andrew.vos@gmail.com>
2022-08-08 17:44:17 +0100

    Switch to material design icon

--- components/CompareAccounts.jsx
+++ components/CompareAccounts.jsx
@@ -224,11 +224,14 @@ export const CompareAccounts = ({ refineSearch, page, query, ...props }) => {
             xmlns="http://www.w3.org/2000/svg"
             width="16"
             height="16"
-            fill="currentColor"
+            preserveAspectRatio="xMidYMid meet"
             className="lg:hidden"
-            viewBox="0 0 16 16"
+            viewBox="0 0 24 24"
           >
-            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+            <path
+              fill="currentColor"
+              d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2Z"
+            ></path>
           </svg>
           <div>Refine your search</div>
         </button>
