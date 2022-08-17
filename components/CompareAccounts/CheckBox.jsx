ff8491449369eec6f0493bd4d3cac8306f7808e9
Andrew Vos <andrew.vos@gmail.com>
2022-08-17 11:16:12 +0100

    Refactor checkbox

--- components/CompareAccounts/CheckBox.jsx
+++ components/CompareAccounts/CheckBox.jsx
@@ -1,16 +1,14 @@
 const CheckBox = ({ id, name, label, value, onChange }) => {
   return (
-    <label className="mr-2 flex items-center py-1 space-x-2 cursor-pointer">
-      <div className="w-8 h-8">
-        <input
-          id={id}
-          name={name}
-          type="checkbox"
-          checked={value}
-          onChange={onChange}
-          className="w-8 h-8 rounded accent-pink-600 focus:shadow-focus-outline outline-none cursor-pointer"
-        />
-      </div>
+    <label className="mr-2 flex items-center space-x-2 cursor-pointer">
+      <input
+        id={id}
+        name={name}
+        type="checkbox"
+        checked={value}
+        onChange={onChange}
+        className="w-8 h-8 rounded accent-pink-600 focus:shadow-focus-outline outline-none cursor-pointer"
+      />
       <div className="text-lg text-gray-900">{label}</div>
     </label>
   );
