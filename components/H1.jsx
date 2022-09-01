5ac8f2c0476a98dca2fced5bec272ee62b63233f
Andrew Vos <andrew.vos@gmail.com>
2022-09-01 09:32:49 +0100

    Fix build warning

--- components/H1.jsx
+++ components/H1.jsx
@@ -5,9 +5,9 @@ const H1 = ({ children }) => {
     <h1
       className="py-8 text-4xl text-blue-800 font-bold mb-8"
       style={{
-        "background-image":
+        backgroundImage:
           "linear-gradient(135deg, #f3f1f3 16.67%, #ffffff 16.67%, #ffffff 50%, #f3f1f3 50%, #f3f1f3 66.67%, #ffffff 66.67%, #ffffff 100%)",
-        "backgroundSize": " 8.49px 8.49px",
+        backgroundSize: " 8.49px 8.49px",
       }}
     >
       <Content>{children}</Content>
